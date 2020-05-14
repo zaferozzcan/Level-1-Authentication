64d7b0e81bad1341e206a40f1c8212bcdb35ebfa
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-05-14 13:19:08 -0400

    Add login and register logic

--- server.js
+++ server.js
@@ -5,9 +5,22 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const PORT = 3000;
 
+// initialize application
 const app = express();
 app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended:true}))
 
+
+
+// database connection
+mongoose.connect("mongodb://localhost:27017/secretsUserDB", { useNewUrlParser: true,useUnifiedTopology: true })
+
+const userSchema = {
+  email:String,
+  password:String
+}
+
+const User = mongoose.model("User", userSchema);
 // home route
 app.get("/", function(req, res){
   console.log("This is text response!");
@@ -20,11 +33,43 @@ app.get("/login", function(req, res){
   res.render("login")
 })
 
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email:username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if (foundUser){
+        if(foundUser.password === password){
+          res.render("secrets")
+        }
+      }
+    }
+  })
+});
 
 // register route
 app.get("/register", function(req, res){
   res.render("register")
-})
+});
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email:req.body.username,
+    password:req.body.password
+  });
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("login")
+    }
+  })
+});
+
+
 
 // secrets route
 app.get("/secrets", function(req, res){
